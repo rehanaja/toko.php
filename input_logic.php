9928172c0a3bd412e86f589e7c2b9cd695fd8484
Muhamad Raihan <muhamadraihan1611@gmail.com>
2025-05-27 16:40:59 +0700

    Tambah file Input_logic.php

---
+++ input_logic.php
@@ -0,0 +1,21 @@
+<?php function garis() {
+    $i = 0;
+    while ($i < 97) {
+        echo "-";
+        $i++;
+} 
+}
+$opsi = [
+    "Peralatan Rumah Tangga" => [
+        "Kulkas" => 1500000,
+        "Kompor" => 500000
+    ],
+    "Peralatan Kantor" => [
+        "Lemari Besi" => 2500000,
+        "Kursi Kantor" => 1500000
+    ]
+    ];
+
+$selectedJenis = $_POST['barang'] ?? null;
+$selectedNama = $_POST['nbarang'] ?? null;
+?>
